19b4c4d868a2ab1e466b56c09e1fefb5206a6707
Rhuan Karlus <rhuankarlus@gmail.com>
2019-03-04 15:15:24 -0300

    Creating abstractService to be used by the service layer

--- project-services/src/main/java/br/com/rk/services/AbstractServico.java
+++ project-services/src/main/java/br/com/rk/services/AbstractServico.java
@@ -2,7 +2,12 @@ package br.com.rk.services;
 
 import br.com.rk.entities.Entidade;
 import br.com.rk.repositories.Repositorio;
+import br.com.rk.services.exception.ServicoException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 /**
  * @author Rhuan Karlus
@@ -18,18 +23,48 @@ public abstract class AbstractServico<E extends Entidade> implements Servico<E> 
     }
 
     @Override
-    public E buscar(Long id) {
-        return null;
+    public E buscar(Long id) throws ServicoException {
+        if (id == null) {
+            throw new ServicoException("O ID informado não pode ser nulo.");
+        }
+
+        return repositorio.findById(id).orElseThrow(() -> new ServicoException("Entidade com ID " + id + " não encontrada."));
     }
 
     @Override
-    public E salvar(E entidade) {
-        return null;
+    public E salvar(E entidade) throws ServicoException {
+        try {
+            return repositorio.save(entidade);
+        } catch (Exception e) {
+            throw new ServicoException("Erro ao tentar persistir a entidade", e);
+        }
     }
 
     @Override
-    public void deletar(Long id) {
+    public void deletar(Long id) throws ServicoException {
+        try {
+            repositorio.deleteById(id);
+        } catch (Exception e) {
+            throw new ServicoException("Erro ao tentar deletar a entidade", e);
+        }
+    }
 
+    @Override
+    public List<E> findAll() throws ServicoException {
+        try {
+            return repositorio.findAll();
+        } catch (Exception e) {
+            throw new ServicoException("Erro ao tentar ler a lista de entidades do banco", e);
+        }
+    }
+
+    @Override
+    public Page<E> findAll(Pageable paginador) throws ServicoException {
+        try {
+            return repositorio.findAll(paginador);
+        } catch (Exception e) {
+            throw new ServicoException("Erro ao tentar ler a página de entidades do banco", e);
+        }
     }
 
 }
