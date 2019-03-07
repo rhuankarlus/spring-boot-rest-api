aef5f75977be7d6a25cc6dd17e335d43d5464ba4
Rhuan Karlus <rhuankarlus@gmail.com>
2019-03-07 00:07:02 -0300

    Starting application from main class of project-api

---
+++ project-api/src/main/java/br/com/rk/Application.java
@@ -0,0 +1,18 @@
+package br.com.rk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author Rhuan Karlus
+ * @since 04/03/2019
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
