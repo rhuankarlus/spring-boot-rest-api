3d784bdab4607873870710fe0620e1eb1c8b85ed
Rhuan Karlus Silva <rhuankarlus@gmail.com>
2019-03-14 16:56:22 -0300

    Refactoring getById method in order to build the required DTO to return

--- project-api/src/main/java/br/com/rk/controller/AbstractCrudControle.java
+++ project-api/src/main/java/br/com/rk/controller/AbstractCrudControle.java
@@ -2,8 +2,8 @@ package br.com.rk.controller;
 
 import br.com.rk.controller.dto.DTO;
 import br.com.rk.controller.dto.ProjectResponse;
-import br.com.rk.converters.Conversor;
 import br.com.rk.entities.Entidade;
+import br.com.rk.services.exception.ServicoException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,26 +11,24 @@ import org.springframework.web.bind.annotation.*;
  * @author Rhuan Karlus
  * @since 04/03/2019
  */
-public abstract class AbstractCrudControle<D extends DTO, E extends Entidade> extends AbstractControle<E> {
+public abstract class AbstractCrudControle<D extends DTO, E extends Entidade> extends AbstractControle<E> implements HasConversor<D, E> {
 
+    @ResponseBody
     @GetMapping(value = "/{id}")
-    public ResponseEntity<ProjectResponse> getById(@PathVariable("id") Long id) {
-        return null;
+    public ResponseEntity<ProjectResponse> getById(@PathVariable("id") Long id) throws ServicoException {
+        return ResponseEntity.ok(ProjectResponse.of(getConversor().toDTO(getServico().findById(id))));
     }
 
     @PostMapping
+    @ResponseBody
     public ResponseEntity<ProjectResponse> salvar(@RequestBody D dto) {
         return null;
     }
 
+    @ResponseBody
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<ProjectResponse> deletar(@PathVariable("id") Long id) {
         return null;
     }
-
-    /**
-     * @return O conversor do DTO para a entidade aceitaos por esse controlador
-     */
-    protected abstract Conversor<D, E> getConversor();
 
 }
