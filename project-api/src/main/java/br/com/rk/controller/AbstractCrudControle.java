717e1ef6372f0ba3ec2fac7718d5c4fe5b27f78b
Rhuan Karlus Silva <rhuankarlus@gmail.com>
2019-03-15 10:41:05 -0300

    Removing hasConversor interface and creating findAll method

--- project-api/src/main/java/br/com/rk/controller/AbstractCrudControle.java
+++ project-api/src/main/java/br/com/rk/controller/AbstractCrudControle.java
@@ -2,13 +2,18 @@ package br.com.rk.controller;
 
 import br.com.rk.controller.dto.DTO;
 import br.com.rk.controller.dto.ProjectResponse;
+import br.com.rk.converters.Conversor;
 import br.com.rk.entities.Entidade;
 import br.com.rk.services.exception.ServicoException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Pageable;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Rhuan Karlus
@@ -16,23 +21,25 @@ import java.awt.print.Pageable;
  */
 public abstract class AbstractCrudControle<D extends DTO, E extends Entidade> extends AbstractControle<E> implements HasConversor<D, E> {
 
+    @Autowired
+    private Conversor<D, E> conversor;
+
     @GetMapping
     @ResponseBody
     public ResponseEntity<ProjectResponse> findAll(@PageableDefault Pageable pageable) throws ServicoException {
-//        return ResponseEntity.ok(ProjectResponse.of(getConversor().toDTO(getServico().findById(id))));
+        return ResponseEntity.ok(convertPage(getServico().findAll(pageable)));
     }
 
     @ResponseBody
     @GetMapping(value = "/{id}")
     public ResponseEntity<ProjectResponse> getById(@PathVariable("id") Long id) throws ServicoException {
-        return ResponseEntity.ok(ProjectResponse.of(getConversor().toDTO(getServico().findById(id))));
+        return ResponseEntity.ok(ProjectResponse.of(conversor.toDTO(getServico().findById(id))));
     }
 
     @PutMapping
     @ResponseBody
     public ResponseEntity<ProjectResponse> persist(@RequestBody D dto) throws ServicoException {
-        return ResponseEntity.ok(ProjectResponse.of(
-                getConversor().toDTO(getServico().persist(getConversor().toEntity(dto)))));
+        return ResponseEntity.ok(ProjectResponse.of(conversor.toDTO(getServico().persist(conversor.toEntity(dto)))));
     }
 
     @ResponseBody
@@ -40,6 +47,18 @@ public abstract class AbstractCrudControle<D extends DTO, E extends Entidade> ex
     public ResponseEntity delete(@PathVariable("id") Long id) throws ServicoException {
         getServico().delete(id);
         return ResponseEntity.ok().build();
+    }
+
+    protected ProjectResponse convertPage(final Page<E> page) {
+        final List<D> dtos = page.getContent().stream().map(conversor::toDTO).collect(Collectors.toList());
+        final ProjectResponse.Pagination pagination = new ProjectResponse.Pagination(
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages(),
+                page.getSort());
+
+        return ProjectResponse.of(null, dtos, pagination);
     }
 
 }
