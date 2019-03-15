f437bf3f725a718cb4ead7f9a0ca6373902c9c23
Rhuan Karlus Silva <rhuankarlus@gmail.com>
2019-03-15 15:57:22 -0300

    Adding global error handler

---
+++ project-api/src/main/java/br/com/rk/exception/GlobalErrorHandler.java
@@ -0,0 +1,16 @@
+package br.com.rk.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * API's Global Error Handler
+ *
+ * @author Rhuan Karlus
+ * @since 15/03/19
+ */
+@ControllerAdvice
+public class GlobalErrorHandler {
+
+    
+
+}
