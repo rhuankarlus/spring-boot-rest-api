58fb3e6b53b2bc24fba7ef454eeed40dbcbc8084
Rhuan Karlus Silva <rhuankarlus@gmail.com>
2019-03-15 11:43:36 -0300

    Inserting persistence context to the abstract custom repository

--- project-repositories/src/main/java/br/com/rk/repositories/AbstractCustomRepository.java
+++ project-repositories/src/main/java/br/com/rk/repositories/AbstractCustomRepository.java
@@ -1,10 +1,21 @@
 package br.com.rk.repositories;
 
 import br.com.rk.entities.ProjectEntity;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
+ * Custom repositories abstraction.
+ *
  * @author Rhuan Karlus
  * @since 15/03/19
  */
+@NoRepositoryBean
 public abstract class AbstractCustomRepository<E extends ProjectEntity> implements CustomRepository<E> {
+
+    @PersistenceContext
+    protected EntityManager entityManager;
+
 }
