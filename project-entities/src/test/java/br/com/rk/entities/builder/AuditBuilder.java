ce2052950ab4a2a66e067c8cd063f3d6c9a38376
Rhuan Karlus Silva <rhuankarlus@gmail.com>
2019-03-28 18:59:16 -0300

    Create tests for auditType and localdatetime converter

---
+++ project-entities/src/test/java/br/com/rk/entities/builder/AuditBuilder.java
@@ -0,0 +1,8 @@
+package br.com.rk.entities.builder;
+
+/**
+ * @author Rhuan Karlus
+ * @since 28/03/19
+ */
+public class AuditBuilder {
+}
---
+++ project-entities/src/test/java/br/com/rk/entities/converters/AuditTypeConverterTest.java
@@ -0,0 +1,8 @@
+package br.com.rk.entities.audit.converters;
+
+/**
+ * @author Rhuan Karlus
+ * @since 28/03/19
+ */
+public class AuditTypeConverterTest {
+}
