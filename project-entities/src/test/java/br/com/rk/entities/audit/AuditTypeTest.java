c667d622ac039fb8b47f12aee4fe178a468cdf3c
Rhuan Karlus Silva <rhuankarlus@gmail.com>
2019-04-04 18:41:09 -0300

    Test for AuditCrudService

--- project-entities/src/test/java/br/com/rk/entities/audit/AuditTypeTest.java
+++ project-entities/src/test/java/br/com/rk/entities/audit/AuditTypeTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
---
+++ project-services/src/test/java/br/com/rk/services/audit/impl/AuditCrudServiceImplTest.java
@@ -0,0 +1,35 @@
+package br.com.rk.services.audit.impl;
+
+import br.com.rk.entities.audit.AuditType;
+import br.com.rk.exceptions.EnumNotFoundException;
+import br.com.rk.services.exception.ServiceException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * @author Rhuan Karlus
+ * @since 04/04/19
+ */
+public class AuditCrudServiceImplTest {
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    private AuditCrudServiceImpl sut;
+
+    @Before
+    public void setUp() throws Exception {
+        sut = new AuditCrudServiceImpl();
+    }
+
+    @Test
+    public void should_throw_exception_when_audit_is_null() throws ServiceException {
+        exceptionRule.expect(ServiceException.class);
+        exceptionRule.expectMessage("The audit object can't be null.");
+
+        sut.validateParams(null, null);
+    }
+
+}
