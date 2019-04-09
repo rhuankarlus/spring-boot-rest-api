e038945832ea0f8828e0e5f0daf0a29017e23810
Rhuan Karlus Silva <rhuankarlus@gmail.com>
2019-04-09 13:48:29 -0300

    Test findAll(pageable)

--- project-services/src/test/java/br/com/rk/services/AbstractCrudServiceTest.java
+++ project-services/src/test/java/br/com/rk/services/AbstractCrudServiceTest.java
@@ -3,12 +3,15 @@ package br.com.rk.services;
 import br.com.rk.entities.ProjectEntity;
 import br.com.rk.repositories.ProjectRepository;
 import br.com.rk.services.exception.ServiceException;
+import br.com.rk.services.factory.PageFactory;
 import br.com.rk.services.factory.ProjectEntityFactory;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.mockito.internal.util.reflection.Whitebox;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -49,9 +52,11 @@ public class AbstractCrudServiceTest {
                 .when(sut)
                 .findById(id);
 
-        sut.findById(id);
-
-        verify(mockRepo, times(1)).findById(id);
+        try {
+            sut.findById(id);
+        } finally {
+            verify(mockRepo, times(1)).findById(id);
+        }
     }
 
     @Test
@@ -101,9 +106,11 @@ public class AbstractCrudServiceTest {
                 .when(sut)
                 .persist(projectEntity);
 
-        sut.persist(projectEntity);
-
-        verify(mockRepo, times(1)).save(projectEntity);
+        try {
+            sut.persist(projectEntity);
+        } finally {
+            verify(mockRepo, times(1)).save(projectEntity);
+        }
     }
 
     @Test
@@ -143,9 +150,11 @@ public class AbstractCrudServiceTest {
                 .when(sut)
                 .delete(id);
 
-        sut.delete(id);
-
-        verify(mockRepo, times(1)).deleteById(id);
+        try {
+            sut.delete(id);
+        } finally {
+            verify(mockRepo, times(1)).deleteById(id);
+        }
     }
 
     @Test
@@ -178,9 +187,11 @@ public class AbstractCrudServiceTest {
                 .when(sut)
                 .findAll();
 
-        sut.findAll();
-
-        verify(mockRepo, times(1)).findAll();
+        try {
+            sut.findAll();
+        } finally {
+            verify(mockRepo, times(1)).findAll();
+        }
     }
 
     @Test
@@ -198,10 +209,63 @@ public class AbstractCrudServiceTest {
 
         final List<ProjectEntity> returnedProjectEntityList = sut.findAll();
 
-        assertNotNull("It shouldn't returned a null list", returnedProjectEntityList);
+        assertNotNull("It shouldn't return a null list", returnedProjectEntityList);
         assertEquals("It should return " + listSize + " elements in the list.", listSize, returnedProjectEntityList.size());
 
         verify(mockRepo, times(1)).findAll();
+    }
+
+    @Test
+    public void should_throw_exception_when_pageable_to_find_all_is_null() throws ServiceException {
+        exceptionRule.expect(ServiceException.class);
+        exceptionRule.expectMessage("Can't find page with a null paginator.");
+
+        doCallRealMethod()
+                .when(sut)
+                .findAll(any());
+
+        sut.findAll(null);
+    }
+
+    @Test
+    public void should_throw_exception_when_find_all_paginated_throw_exception() throws ServiceException {
+        exceptionRule.expect(ServiceException.class);
+        exceptionRule.expectMessage("Error when trying to read the entities page from database");
+
+        final Pageable pageable = PageFactory.buildSimplePageable();
+        final ProjectRepository mockRepo = mock(ProjectRepository.class);
+        when(mockRepo.findAll(any(Pageable.class))).thenThrow(Exception.class);
+        Whitebox.setInternalState(sut, "projectRepository", mockRepo);
+
+        doCallRealMethod()
+                .when(sut)
+                .findAll(any());
+
+        try {
+            sut.findAll(pageable);
+        } finally {
+            verify(mockRepo, times(1)).findAll(pageable);
+        }
+    }
+
+    @Test
+    public void should_return_paginated_list_when_find_all_paginated_execute_correctly() throws ServiceException {
+        final Page<ProjectEntity> projectEntitiesPage = PageFactory.buildSimplePage(ProjectEntityFactory::buildSimpleEntityWithoutId);
+        final Pageable pageable = PageFactory.buildSimplePageable();
+        final ProjectRepository mockRepo = mock(ProjectRepository.class);
+        when(mockRepo.findAll(any(Pageable.class))).thenReturn(projectEntitiesPage);
+        Whitebox.setInternalState(sut, "projectRepository", mockRepo);
+
+        doCallRealMethod()
+                .when(sut)
+                .findAll(any(Pageable.class));
+
+        final Page<ProjectEntity> returnedProjectEntitiesPage = sut.findAll(pageable);
+
+        assertNotNull("The list returned shouldn't be null.", returnedProjectEntitiesPage);
+        assertEquals("It should return only 5 elements.", 5, returnedProjectEntitiesPage.getContent().size());
+
+        verify(mockRepo, times(1)).findAll(pageable);
     }
 
 }
---
+++ project-services/src/test/java/br/com/rk/services/factory/PageFactory.java
@@ -0,0 +1,42 @@
+package br.com.rk.services.factory;
+
+import org.springframework.data.domain.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+
+/**
+ * Simple factory to build test helpful pageable
+ *
+ * @author Rhuan Karlus
+ * @since 09/04/2019
+ */
+public class PageFactory {
+
+    private static final int MAX_PAGE_SIZE = 5;
+
+    public static Pageable buildSimplePageable() {
+        return buildPageable(1, MAX_PAGE_SIZE, null);
+    }
+
+    public static Pageable buildPageable(final int page, final int size, final Sort sortObject) {
+        if (sortObject == null) {
+            return PageRequest.of(page, size);
+        }
+
+        return PageRequest.of(page, size, sortObject);
+    }
+
+    public static <T> Page<T> buildSimplePage(final Supplier<T> randomObjectCreator) {
+        return buildPage(randomObjectCreator, buildSimplePageable(), MAX_PAGE_SIZE);
+    }
+
+    public static <T> Page<T> buildPage(final Supplier<T> randomObjectCreator, final Pageable pageable, int size) {
+        final List<T> content = new ArrayList<>();
+        IntStream.rangeClosed(1, size).forEach(value -> content.add(randomObjectCreator.get()));
+        return new PageImpl<>(content, pageable, size);
+    }
+
+}
