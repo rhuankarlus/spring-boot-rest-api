cfbf78e5c062b124e439b889d4250aa8a89d8f85
Rhuan <rhuankarlus@gmail.com>
2019-05-22 11:47:10 -0300

    #12 Create user domain tables

--- project-api/src/test/resources/drop_data.sql
+++ project-api/src/test/resources/drop_data.sql
@@ -1,4 +1,10 @@
+-- relation tables
+DELETE FROM role_permission; ALTER SEQUENCE id_seq_role_permission RESTART WITH 1;
+DELETE FROM user_permission; ALTER SEQUENCE id_seq_user_permission RESTART WITH 1;
+DELETE FROM user_role; ALTER SEQUENCE id_seq_user_role RESTART WITH 1;
+
+-- final tables
 DELETE FROM audit; ALTER SEQUENCE id_seq_audit RESTART WITH 1;
 DELETE FROM permission; ALTER SEQUENCE id_seq_permission RESTART WITH 1;
 DELETE FROM role; ALTER SEQUENCE id_seq_role RESTART WITH 1;
-DELETE FROM role_permission; ALTER SEQUENCE id_seq_role_permission RESTART WITH 1;
+DELETE FROM user; ALTER SEQUENCE id_seq_user RESTART WITH 1;
